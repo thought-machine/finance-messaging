de3a622e401d5aabae23e8f73d653e3ae0bfe18f
Michael Dowling <Michael.Dowling@minderupt.com>
2018-02-25 15:42:02 -0800

    Mass initial commit

---
+++ iso20022/CashEntitlement1.go
@@ -0,0 +1,12 @@
+package iso20022
+
+// Provides information about cash entitlements.
+type CashEntitlement1 struct {
+
+	// Entitled cash amount.
+	CashAmount *ActiveCurrencyAndAmount `xml:"CshAmt"`
+}
+
+func (c *CashEntitlement1) SetCashAmount(value, currency string) {
+	c.CashAmount = NewActiveCurrencyAndAmount(value, currency)
+}
---
+++ iso20022/Linkages40.go
@@ -0,0 +1,13 @@
+package iso20022
+
+// Information related to a linked transaction.
+type Linkages40 struct {
+
+	// Reference to the linked transaction.
+	Reference *References47Choice `xml:"Ref"`
+}
+
+func (l *Linkages40) AddReference() *References47Choice {
+	l.Reference = new(References47Choice)
+	return l.Reference
+}
